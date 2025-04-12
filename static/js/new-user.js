2f6db51fb82d49593bb1bc4f48f1334960bee0c9
Admin9705 <9705@duck.com>
2025-04-12 04:38:03 -0400

    update

---
+++ static/js/new-user.js
@@ -0,0 +1,23 @@
+/**
+ * Huntarr - User Settings Page
+ * Handles user profile management functionality
+ */
+
+document.addEventListener('DOMContentLoaded', function() {
+    // User management functionality will be implemented here
+    // Most of the functionality is already included inline in the HTML page
+    
+    // This file serves as a placeholder for any additional user management 
+    // functionality that might be needed in the future
+    
+    console.log('User settings page loaded');
+    
+    // The following functions could be implemented here and imported in the HTML
+    // instead of using inline JavaScript:
+    // - loadUserInfo()
+    // - changeUsername()
+    // - changePassword()
+    // - setupTwoFactor()
+    // - verifyTwoFactor()
+    // - disableTwoFactor()
+});
